5c3bb27f45e4023895e42bfed261d371f01ae284
sujay-itilite <sujay.suresh@itilite.com>
2022-07-08 01:03:18 +0530

    adding node-sass and alpha component

--- src/components/Alpha.js
+++ src/components/Alpha.js
@@ -1,8 +1,9 @@
 import React from "react";
+import '../assets/css/custom.sass'
 
 const Alpha = () => {
     return(
-        <p>This is the Alpha component</p>
+        <p className="custom">This is the Alpha component</p>
     )
 }
 
